073c0f1afcebb73a61fe33a1e0884538f8e36a0c
markiyan2009 <93789450+markiyan2009@users.noreply.github.com>
2024-08-08 18:25:28 +0300

    FInish project

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from myapp import views
 urlpatterns = [
     path("",views.NoteListView.as_view(), name='notes-list'),
-    path('<int:pk>/', views.NoteDetailView.as_view(), name="note"),
+    path('<int:pk>/', views.NoteDetailView.as_view(), name="note-detail"),
     path("create-note", views.CreateNoteView.as_view(), name='add-note')
 ]
